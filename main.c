04128094900510ff94c8319da566c306ac77f3ff
mirollawa2l <89114408+mirollawa2l@users.noreply.github.com>
2023-12-19 15:39:53 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,30 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-float fact(int n)
-{
-    int i,f=1;
-    {
+#include <math.h>
+#include <string.h>
 
-        for(i=n; i>0; i--)
-        {
-            f*=i;
-        }
-    }
-
-
-    
-    return f;
-}
 int main()
 {
-    float sum=0;
-    int n,i;
-    printf("Enter the number of terms:");
-    scanf("%d",&n);
-    for(i=0; i<2*n; i+=2)
-    {
-        sum+=pow(i+1,2)/fact(i);
-    }
-    printf("%f",sum);
+    
+    
+    
+    
     return 0;
 }
+
